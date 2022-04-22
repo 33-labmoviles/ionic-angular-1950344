ea5ba566334b141e7f5506275f6965bf8c30dc04
OmarAlexGarza <oagl03@outlook.com>
2022-04-21 22:20:45 -0500

    26feb-practica3

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,79 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  constructor() {}
+
+  alumnos = [
+
+    {"Nombre": "Abraham",
+     "Apellido": "Ramirez",
+     "Matricula": "ABC123"
+    },
+    {"Nombre": "Aylin",
+     "Apellido": "Demetci",
+     "Matricula": "ABC123"
+    },
+    {"Nombre": "Brian",
+     "Apellido": "Esquivel",
+     "Matricula": "ABC123"
+    },
+    {"Nombre": "Diego",
+     "Apellido": "Davila",
+     "Matricula": "ABC123"
+    },
+    {"Nombre": "Diego",
+     "Apellido": "Jasso",
+     "Matricula": "ABC123"
+    },
+    {"Nombre": "Julio",
+     "Apellido": "Manuel",
+     "Matricula": "ABC123"
+    },
+    {"Nombre": "Luis",
+     "Apellido": "Villanueva",
+     "Matricula": "ABC123"
+    },
+    {"Nombre": "Luis",
+     "Apellido": "Tamez",
+     "Matricula": "ABC123"
+    },
+    {"Nombre": "Manuel",
+     "Apellido": "Juarez",
+     "Matricula": "ABC123"
+    },
+    {"Nombre": "Rogel",
+     "Apellido": "Guel",
+     "Matricula": "ABC123"
+    },
+    {"Nombre": "Sergio",
+     "Apellido": "Gutierrez",
+     "Matricula": "ABC123"
+    },
+    {"Nombre": "Omar",
+     "Apellido": "Garza",
+     "Matricula": "ABC123"
+    },
+    {"Nombre": "Juan",
+     "Apellido": "Torres",
+     "Matricula": "ABC123"
+    },
+    {"Nombre": "Bryan",
+     "Apellido": "Castillo",
+     "Matricula": "ABC123"
+    },
+    {"Nombre": "Emiliano",
+     "Apellido": "Rodriguez",
+     "Matricula": "ABC123"
+    },
+  
+  ];
+
+
+
+}
